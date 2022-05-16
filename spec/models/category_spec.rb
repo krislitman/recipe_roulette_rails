2e5c910aad1ba614949dfddc18052db1766b6398
Kris Litman <kris.d.litman@gmail.com>
2022-05-15 21:27:45 -0400

    Category Spec passing

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Category, type: :model do
+    describe "Validations" do
+        it { should validate_presence_of :name }
+    end
+    describe "Relationships" do
+    end
+end
