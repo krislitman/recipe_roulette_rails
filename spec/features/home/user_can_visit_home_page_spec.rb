f292ad46462fb8a3b0466a0362b641c36970ad73
Kris Litman <kris.d.litman@gmail.com>
2022-05-18 21:23:24 -0400

    Added home controller, route, and view

--- spec/features/home/user_can_visit_home_page_spec.rb
+++ spec/features/home/user_can_visit_home_page_spec.rb
@@ -2,10 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Home Page", type: :feature do
     describe "As a visitor visting the home page" do
-        it "I am able to visit the page /" do
+        it "I am able to visit the home page" do
             visit root_path
 
             expect(current_path).to eq(root_path)
+            expect(page).to have_content("Welcome to Recipe Roulette")
         end
     end
 end
