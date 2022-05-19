fab125b6c0a43b0cee0bb53260f7dd30d7833eb5
Kris Litman <kris.d.litman@gmail.com>
2022-05-18 21:54:06 -0400

    Created recipe factories

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
     factory :recipe do
-      sequence(:name) { |n| "Item #{n}" }
-      sequence(:description) { |n| "Description #{n}" }
-      sequence(:unit_price) { |n| "Unit price #{n}" }
-      merchant
+      sequence(:name) { |n| "#{Faker::Food.dish}} #{n}" }
+      serves { "#{Faker::Number.between(from: 1, to: 10)}" }
+      prep_time { "#{Faker::Number.between(from: 1, to: 60)} Minutes" }
+      cooking_time { "#{Faker::Number.between(from: 1, to: 60)} Minutes" }
+      ingredients { "#{Faker::Food.ingredient}" }
+      instructions { "#{Faker::Hacker.say_something_smart}" }
+      author { "#{Faker::Movies::LordOfTheRings.character}" }
     end
   end
