6c2c67d500ce131aea5498c2ad613b3b3047dd0c
Kris Litman <kris.d.litman@gmail.com>
2022-06-17 21:32:03 -0400

    Added in charge controller and views

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,24 @@
+class ChargesController < ApplicationController
+  def new
+  end
+
+  def create
+    @amount = 100
+
+    customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Test Customer Charge',
+        currency: 'usd'
+    )
+
+    rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_charge_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     root to: "home#index"
 
-#   Recipes
+    #   Recipes
     resources :recipes, only: [:index, :show]
+
+    #   Stripe
+    resources :charges, only: [:new, :create]
 end
