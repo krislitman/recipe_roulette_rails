9af62a129d412f94047d5cd5eab686ba645e6283
Kris Litman <kris.d.litman@gmail.com>
2022-10-31 22:14:08 -0400

    Updated to rails 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root to: 'home#index'
 
-  #   Recipes
+  root to: "home#index"
+
   resources :recipes
 end
